fca758f07ff718430adf8906d3e2b865857e013b
Wolferine3 <68408026+Wolferine3@users.noreply.github.com>
2020-07-16 22:17:25 -0400

    Add files via upload

---
+++ test amq.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         test amq
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://animemusicquiz.com/*
+// @grant        none
+// ==/UserScript==
+
+let test = "test23";
