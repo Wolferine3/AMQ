a2d25f2351e833b6955270b4198144a5b4881375
Wolferine3 <68408026+Wolferine3@users.noreply.github.com>
2020-07-17 16:46:36 -0400

    Add files via upload

---
+++ Database Questions.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Database Questions
+// @namespace    https://github.com/Wolferine3
+// @version      1.0
+// @description  Database for questions on AMQ trivia
+// @author       Wolferine
+// @match        https://animemusicquiz.com/*
+// ==/UserScript==
+
+const question = [];
