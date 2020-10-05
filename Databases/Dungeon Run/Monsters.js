30ceae64206edfef1320b55e7c8dbfb8a6be7c53
Wolferine3 <68408026+Wolferine3@users.noreply.github.com>
2020-10-04 22:02:56 -0400

    Update Monsters.js

--- Databases/Dungeon Run/Monsters.js
+++ Databases/Dungeon Run/Monsters.js
@@ -1,4 +1,13 @@
-let monsters = ["Spider",
+// ==UserScript==
+// @name         Database Monsters
+// @namespace    https://github.com/Wolferine3
+// @version      1.0
+// @description  Database for monster on AMQ Dungeon Run
+// @author       Wolferine
+// @match        https://animemusicquiz.com/*
+// ==/UserScript==
+
+const monsters = ["Spider",
 "Bee",
 "Rat",
 "Bat",
